eb904ce9b53708334c0caddf4902b97933acd573
Vhkan <vhkan@yahoo.com>
2023-10-26 15:11:04 -0600

    Add ejs template engine to the app

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //Default port
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,7 +21,7 @@ app.get('/hello',(req, res) => {
   res.send('<html><body>Hello<b>World</b></body></html>\n');
 });
 
+
 app.listen(PORT, () => {
   console.log(`App is listening on ${PORT}!`);
 });
-  
