dbfbfdb23220f02dbab53fcda360e29208938df0
Vhkan <vhkan@yahoo.com>
2023-11-03 22:12:57 -0600

    Add test folder, helpersTest.js file with test cases to test helper.js functionality

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, generateRandomString, urlsForUser } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -15,10 +15,42 @@ const testUsers = {
   }
 };
 
+//Links storage "DB"
+const testUrlDatabase = {
+  b6UUxQ: {
+    longURL: "https://www.epl.ca",
+    userID: "aJ45lW",
+  },
+  i3LLGr: {
+    longURL: "https://www.ufa.ca",
+    userID: "aJ55lW",
+  },
+};
+
+//Chararters for generateRandomString function
+let characterSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+let strLen = 6;
+
+
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
-    // Write your assert statement here
+    assert.strictEqual(user.id, expectedUserID);
+  }); 
+});
+
+describe('generateRandomString', function() {
+  it('should generate an 6 digit alpanumeric low + capital letters string', function() {
+    const alphaNumStr = generateRandomString(strLen,characterSet);
+    assert.strictEqual(alphaNumStr.length, 6);
+  });
+});
+
+describe('urlsForUser', function() {
+  it('Should return an empty object if there is no user id in the DB', function() {
+    const randomUserId = 'aJ10lW"';
+    const userUrls = urlsForUser(testUrlDatabase, randomUserId);
+    assert.deepEqual(userUrls, {});
   });
 });
